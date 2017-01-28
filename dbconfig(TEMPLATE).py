bb38ee2f60234ac78e4d84d8ccc21fb04da92ba8
mmmatthew <mmmatthew@users.noreply.github.com>
2017-01-28 17:28:18 +0100

    Added useful comments

--- dbconfig(TEMPLATE).py
+++ dbconfig(TEMPLATE).py
@@ -1,5 +1,10 @@
+# ip address of remote machine
 address = ""
+# your SSH username
 username = ""
+# your SSH password
 password = ""
+# the directory in which data should be stored on the remote machine before loading into database
 data_dir = ""
+# the port used by the database on the remote machine
 port = "4200"
